69d91c4a87b055f1ade6673c6543ee8c2bd13d98
Theophilus Aika <designxperienz@gmail.com>
2025-10-16 15:55:29 +0100

    Initial setup

---
+++ components/Layouts/Navbar.tsx
@@ -0,0 +1,30 @@
+import { navLinks } from '../../constants'
+
+const Navbar = () => {
+  return (
+    <header className="px-6 py-4 border-b border-gray-800">
+        <nav className="flex justify-between align-center">
+            <img src="/logo.svg" alt="Apple" />
+
+            <ul className="flex gap-6">
+                {navLinks.map((link, index) => (
+                    <li key={index}>
+                        <a href="#">{link.name}</a>
+                    </li>
+                ))}
+            </ul>
+
+            <div className="flex-center gap-3">
+                <button className="icon-search">
+                    <img src="/search.svg" alt="Search" />
+                </button>
+                <button className="icon-search">
+                    <img src="/cart.svg" alt="Cart" />
+                </button>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ constants/index.js
@@ -0,0 +1,10 @@
+export const navLinks = [
+  { name: "Store", href: "/" },
+  { name: "Mac", href: "/mac" },
+  { name: "iPad", href: "/ipad" },
+  { name: "iPhone", href: "/iphone" },
+  { name: "Watch", href: "/watch" },
+  { name: "AirPods", href: "/air-pods" },
+  { name: "Vision", href: "/vision" },
+  { name: "Support", href: "/support" },
+];
